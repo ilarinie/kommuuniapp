deabaaacd04789e9625f4c2adcda638a87bda689
ilarinie <ilari.nieminen@outlook.com>
2016-04-22 09:49:50 +0300

    added validations to chore

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -1,6 +1,9 @@
 class Chore < ActiveRecord::Base
   has_many :tasks, dependent: :destroy
   has_many :users, through: :chore_users
+  
+  validates :name, presence: true, length: { in:2..30 }
+  
 
   def to_s
     self.name
