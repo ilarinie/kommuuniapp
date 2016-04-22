de513561eb73859fab848b187a2e747a55dd7c81
ilarinie <ilari.nieminen@outlook.com>
2016-04-22 10:10:45 +0300

    added validations to purchase

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,6 +1,10 @@
 class Purchase < ActiveRecord::Base
   belongs_to :user
   belongs_to :purchase_category
+  
+  validates :user_id, presence: true
+  validates :price, greater_than:0
+  validates :description, length: {in: 3..100 }
 
   self.per_page = 10
 end
