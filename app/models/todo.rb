1ec2cdf308d035c6a736cfdc6622c2d787a90a18
ilarinie <ilari.nieminen@outlook.com>
2016-04-22 10:45:55 +0300

    added validations to todo

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -3,6 +3,8 @@ class Todo < ActiveRecord::Base
   has_one :todo_solution
   has_many :todo_comments
   belongs_to :todo_category
+  
+  validates :title, length: {in: 3..50 }
 
 
   self.per_page = 10
