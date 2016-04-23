f6eec60d0e9f378cbc2d1718c14c3323803fcc6d
lemanne <ilari.nieminen@outlook.com>
2016-04-23 16:53:10 +0300

    more tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  describe "GET methods" do
   before :each do
     @user = FactoryGirl.create(:user, admin:true,deactivated:false)
     session[:user_id] = @user.id
@@ -17,11 +18,42 @@ RSpec.describe UsersController, type: :controller do
     expect(response).to render_template(:manage)
   end
 
+end
+  describe "UPDATE methods" do
+    before :each do
+      request.env["HTTP_REFERER"] = root_url
+    end
+
   it 'if admin should be able to deactivate users' do
-    user2 = FactoryGirl.create(:user, deactivated:true)
+    u = FactoryGirl.create(:user, admin:true)
+    session[:user_id] = u.id
+    user2 = FactoryGirl.create(:user, deactivated:false)
     delete :destroy, id:user2.id
+    expect(user2.deactivated).to eq(false)
+  end
+
+  it 'if admin should be able to activate users' do
+    u = FactoryGirl.create(:user, admin:true)
+    session[:user_id] = u.id
+    user2 = FactoryGirl.create(:user, deactivated:true)
+    post :activate, id:user2.id
     expect(user2.deactivated).to eq(true)
   end
 
+  it 'if admin should be able to create user' do
+    u = FactoryGirl.create(:user, admin:true)
+    session[:user_id] = u.id
+    expect do
+    post :create, user: FactoryGirl.attributes_for(:user)
+  end.to change {User.count}.by(1)
+end
 
+  it 'should be able to edit information' do
+    u = FactoryGirl.create(:user, admin:true)
+    session[:user_id] = u.id
+    put :update, id:u.id, user: FactoryGirl.attributes_for(:chore, name:"test2")
+    expect(User.find(u.id).name).to eq("test2")
+  end
+
+end
 end
