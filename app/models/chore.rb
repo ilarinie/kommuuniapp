af34f01896a7216a848b289ae43d72afc58a426b
ilarinie <ilari.nieminen@outlook.com>
2016-04-29 11:34:40 +0300

    Update chore.rb

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -2,6 +2,8 @@ class Chore < ActiveRecord::Base
   has_many :tasks, dependent: :destroy
   has_many :users, through: :chore_users
   
+  attr_accessor :priorities
+  
   validates :name, presence: true, length: { in:2..30 }
   
 
