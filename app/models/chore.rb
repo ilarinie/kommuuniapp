2f1791c40a971a5825ee48a169713788f9f413fd
ilarinie <ilari.nieminen@outlook.com>
2016-04-29 11:53:04 +0300

    Update chore.rb

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -10,4 +10,14 @@ class Chore < ActiveRecord::Base
   def to_s
     self.name
   end
+  
+  def prioritycalc multiplier hours
+    case multiplier
+    when 'hours'
+      hours
+    when 'days'
+      hours*24
+    when 'weeks'
+      hours*24*7
+    end
 end
